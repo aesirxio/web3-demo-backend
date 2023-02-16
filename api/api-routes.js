0029d778c09016d5146ff3cb396393294b1b82c1
Vu Cong Thuong <thuong@redweb.dk>
2023-02-16 14:20:21 +0700

    Add the image to the creation form

--- api/api-routes.js
+++ api/api-routes.js
@@ -3,29 +3,6 @@
 const router = require("express").Router();
 // const concordium = new Concordium();
 const multer = require('multer');
-const mime   = require('mime-types');
-const fs     = require('fs');
-let dirImg   = './image';
-
-if (!fs.existsSync(dirImg)){
-    fs.mkdirSync(dirImg);
-}
-const storage = multer.diskStorage({
-  destination: function (req, image, cb) {
-    cb(null, dirImg);
-  },
-  filename: function (req, image, cb) {
-    let ext = mime.extension(image.mimetype);
-    let imageName = image.originalname;
-
-    if (typeof req.body.name !== 'undefined') {
-        imageName = req.body.name + '_' + Date.now() + '.' + ext;
-    }
-
-    cb(null, imageName.replace(/\s/g, '_'));
-  },
-});
-const upload = multer({ storage: storage });
 
 // Set default API response
 router.get("/", function (req, res) {
@@ -40,7 +17,7 @@ router.route("/account/v1/:account/nonce").get(accountController.getNonce);
 // Product routes
 const productController = require("./controllers/productController");
 
-router.route("/product/v1").post(upload.single('main_image'), productController.add);
+router.route("/product/v1").post(multer().single('main_image'), productController.add);
 router.route("/product/v1/:account").get(productController.list);
 
 // Export API routes
--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -8,14 +8,29 @@ const Account = require("../models/accountModel");
 const Concordium = require("../web3/concordium");
 const concordium = new Concordium();
 
+const mime   = require('mime-types');
+const multer = require("multer");
+let dirImg   = './image/';
+
 exports.add = async (req, res) => {
+
+  if (!fs.existsSync(dirImg)){
+    fs.mkdirSync(dirImg);
+  }
+    let ext = mime.extension(req.file.mimetype);
+    let imageName = req.body.name.replace(/\s/g, '_') + '_' + Date.now() + '.' + ext;
+
+  fs.writeFile(dirImg + imageName, req.file.buffer, (err) => {
+    if (err) throw err;
+  });
+
   await Product.create({
     sku: req.body.sku,
     name: req.body.name,
     description: req.body.description,
     nftBlock: req.body.block,
     nftToken: req.body.token,
-    main_image: req.file.path,
+    main_image: dirImg + imageName,
   });
   res.status(201);
   res.json({ success: true });
